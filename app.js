486251c61e4b0e0e6994b24c97e502b866430068
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-18 22:50:46 +0200

    passport riesenie implementovane - level 5 ci 6 uz neviem, je tam cookie ktore kontroluje cije prihlaseny uzvatel stale ku session, uzlozi cookie

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-
-
+const session = require('express-session')
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
@@ -18,6 +16,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(session({
+  secret: "Moje male tajomstvo.",
+  resave: false,
+  saveUnitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
@@ -29,7 +36,14 @@ const userSchema = new mongoose.Schema({
 });
 
 
+userSchema.plugin(passportLocalMongoose);
+
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -43,49 +57,57 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function(err) {
+  User.register({
+      username: req.body.username
+    }, req.body.password,
+    function(err,user) {
       if (err) {
         console.log(err);
+        res.redirect("/register");
       } else {
-        res.render("secrets");
+        passport.authenticate("local")(req, res, function() {
+          res.redirect("/secrets");
+        });
       }
     });
-  });
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({
-    email: username
-  }, function(err, found) {
-
-    if (found) {
-      bcrypt.compare(password, found.password, function(err, result) {
-
-
-        if (result === true) {
-          res.render("secrets");
-        } else {
-          console.log("wrong password");
-        };
-      });
-    } else {
-      console.log("Unauthorised");
-    }
-
-  });
+const user = new User({
+  username: req.body.username,
+  password: req.body.password
 });
+
+req.login(user, function(err){
+if (err) {
+  console.log(err);
+} else {
+  passport.authenticate("local")(req, res, function() {
+    res.redirect("/secrets");
+  });
+}
+
+});
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started: 3000.");
